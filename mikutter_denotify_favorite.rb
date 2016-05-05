0c3a5f883d15d85af3b5496341ea65ac047f70a1
vicco <mm.souichi@gmail.com>
2016-05-05 19:45:15 +0900

    command.rbを参考にCanFavoriteAnyに変更

--- mikutter_denotify_favorite.rb
+++ mikutter_denotify_favorite.rb
@@ -3,8 +3,9 @@
 Plugin.create(:mikutter_denotify_favorite) do
   command(:denotify_favorite,
           name: "非通知ふぁぼ",
-          condition: Plugin::Command[:HasOneMessage],
+          condition: Plugin::Command[:CanFavoriteAny],
           visible: true,
+          icon: Skin.get("unfav.png"),
           role: :timeline) do |opt|
     message = opt.messages.first
     (Service.primary.twitter/'favorites/create').message({:id => message.id})
